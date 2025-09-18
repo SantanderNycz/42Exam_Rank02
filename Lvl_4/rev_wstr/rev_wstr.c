00dbb03c9f1cc45c805a433c5ca2e06311cb3ca8
Leo Santander <santandernycz.ls@gmail.com>
2025-09-18 18:11:58 +0100

    Atualização de funções

--- Lvl_4/rev_wstr/rev_wstr.c
+++ Lvl_4/rev_wstr/rev_wstr.c
@@ -2,32 +2,39 @@
 
 int main(int ac, char **av)
 {
+	int i = 0;
+	int j = 0;
 	int start;
 	int end;
-	int i = 0;
-		
-	if(ac == 2)
-	{   
-		while(av[1][i] != '\0')
+
+	if (ac == 2)
+	{
+		while (av[1][i])
 			i++;
-		while(i >= 0)
+		i--;
+
+		while (i >= 0)
 		{
-			while( av[1][i] == '\0' || av[1][i] == ' ' || av[1][i] == '\t')
+			while (i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
 				i--;
 			end = i;
-			while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+			while (i >= 0 && (av[1][i] != ' ' && av[1][i] != '\t' && av[1][i] != '\0'))
 				i--;
 			start = i + 1;
-			int  flag;
-			flag = start;
-			while(start <= end)
+			
+			if (start <= end)
 			{
-				write (1, &av[1][start],1);
-				start++;		
+				j = start;
+				while (j <= end)
+				{
+					write(1, &av[1][j], 1);
+					j++;
+				}
+				if (i >= 0)
+					write (1, " ", 1);
 			}
-			if (flag !=0)
-				write(1, " ", 1);
 		}
 	}
 	write(1, "\n", 1);
+	return 0;
 }
