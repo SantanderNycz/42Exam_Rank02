1d7a7dc3f1d5e22defe1730e3d4da91ab3a0b565
Leo Santander <santandernycz.ls@gmail.com>
2025-10-01 16:42:53 +0100

    atualização sort_list

--- Lvl_4/sort_list/sort_list.c
+++ Lvl_4/sort_list/sort_list.c
@@ -1,30 +1,50 @@
 #include <stdlib.h>
 
-typedef struct s_list t_list;
-
-struct s_list
+typedef struct	s_list
 {
-	int     data;
-	t_list  *next;
-};
+	int	data;
+	struct s_list *next;
 
-t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+}	t_list;
+
+void ft_swap(int *a, int *b)
 {
-	int swap;
-	t_list *tmp;
+	int tmp;
 
-	tmp = lst;
-	while (lst && lst->next)
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int asc(int a, int b)
+{
+	return (a <= b);
+}
+
+int desc(int a, int b)
+{
+	return (a >= b);
+}
+
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+{
+	t_list *lst_start;
+	int tmp;
+
+	if (!lst)
+		return NULL;
+
+	lst_start = lst;
+	while (lst->next)
 	{
-		if ((cmp(lst->data, lst->next->data)) == 0)
+		if ((*cmp)(lst->data, lst->next->data) == 0)
 		{
-			swap = lst->data;
-			lst->data = lst->next->data;
-			lst->next->data = swap;
-			lst = tmp;
+			ft_swap(&lst->data, &lst->next->data);
+			lst = lst_start;
 		}
 		else
-			lst = lst->next;
+			lst = lst_start;
 	}
-	return (tmp);
+	lst = lst_start;
+	return (lst);
 }
