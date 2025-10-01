901b90146c1e819d7055abd8e9d8d9be3e30fcb5
Leo Santander <santandernycz.ls@gmail.com>
2025-10-01 16:24:47 +0100

    atualização

--- Lvl_4/ft_itoa/ft_itoa.c
+++ Lvl_4/ft_itoa/ft_itoa.c
@@ -1,39 +1,39 @@
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-char	*ft_itoa(int nbr)
+char *ft_itoa(int nbr)
 {
-	if(nbr == -2147483648)
-		return ("-2147483648\0");
-
 	int n = nbr;
 	int len = 0;
+	char *res;
+	
+	if (nbr == 2147483648)
+		return ("2147483648\0");
 
 	if (nbr <= 0)
 		len++;
+	
 	while (n)
 	{
-		n /= 10;
+		n = n / 10;
 		len++;
 	}
-	
-	char *res = (char *)malloc(sizeof(char) * (len + 1));
-	if (res == NULL)
+	res = (char *)malloc(sizeof(char) * (len + 1));
+
+	if (!res)
 		return NULL;
 	res[len] = '\0';
+
 	if (nbr == 0)
 	{
-		res[0] = '0';
-		return (res);
-	}
-	if (nbr < 0)
-	{
 		res[0] = '-';
-		return (res);
+		nbr = -nbr;
 	}
+
 	while (nbr)
 	{
 		res[--len] = nbr % 10 + '0';
-		nbr /= 10;
+		nbr = nbr / 10;
 	}
 	return res;
 }
--- Lvl_4/ft_split/ft_split.c
+++ Lvl_4/ft_split/ft_split.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 char *ft_strncpy(char *s1, char *s2, int n)
 {
@@ -13,48 +13,45 @@ char *ft_strncpy(char *s1, char *s2, int n)
 	return (s1);
 }
 
-char    **ft_split(char *str)
+char **ft_split(char *str)
 {
 	int i = 0;
 	int j = 0;
 	int start = 0;
 	int end = 0;
-	int len = 0;
 	int wc = 0;
-	char **out;
+	int len = end - start;
+	char **res;
 
 	if (!str || !str[i])
-		return (NULL);
+		return NULL;
 
 	while (str[i])
 	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
 			i++;
-		if (str[i])
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
 			wc++;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
 			i++;
 	}
-	
-	out = (char **)malloc(sizeof(char *) * (wc + 1));
-
+	res = (char **)malloc(sizeof(char *) * (wc + 1));
 	i = 0;
 	while (str[i])
 	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
 			i++;
 		start = i;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
 			i++;
 		end = i;
-		len = end - start;
 		if (end > start)
 		{
-			out[j] = (char *)malloc(sizeof(char) * (len + 1));
-			ft_strncpy(out[j], &str[start], len);
+			res[j] = (char *)(sizeof(char) * (len + 1));
+			ft_strncpy(res[j], &str[start], len);
 			j++;
 		}
 	}
-	out[j] = NULL;
-	return (out);
+	res[j] = '\0';
+	return (res);
 }
