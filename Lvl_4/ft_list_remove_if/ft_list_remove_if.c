aa872dfad7e7bf719627c1c5f9f40c65b9cff31e
Leo Santander Nycz <121593322+SantanderNycz@users.noreply.github.com>
2025-11-07 13:56:30 +0000

    Refactor ft_list_remove_if for clarity and efficiency

--- Lvl_4/ft_list_remove_if/ft_list_remove_if.c
+++ Lvl_4/ft_list_remove_if/ft_list_remove_if.c
@@ -12,19 +12,16 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
 	if (begin_list == NULL || *begin_list == NULL)
 		return ;
-	
-	t_list *cur = *begin_list;
 
-	if (cmp(cur->data, data_ref) == 0)
+	if (cmp((*begin_list)->data, data_ref) == 0)
 	{
-		*begin_list = cur->next;
+		t_list *cur = *begin_list;
+		*begin_list = (*begin_list)->next;
 		free(cur);
 		ft_list_remove_if(begin_list, data_ref, cmp);
 	}
 	else
 	{
-		cur = *begin_list;
-		ft_list_remove_if(&cur->next, data_ref, cmp);
+		ft_list_remove_if(&(*begin_list)->next, data_ref, cmp);
 	}
-
 }
