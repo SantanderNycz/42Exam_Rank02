ab4804367f56ffe9eaf2f75aca874854118cda19
Leo Santander Nycz <121593322+SantanderNycz@users.noreply.github.com>
2025-11-07 13:22:56 +0000

    Update ft_list_remove_if.c

--- Lvl_4/ft_list_remove_if/ft_list_remove_if.c
+++ Lvl_4/ft_list_remove_if/ft_list_remove_if.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
-//#include "ft_list.h"
+#include "ft_list.h"
 
+/*
 typedef struct      s_list
 {
     struct s_list   *next;
     void            *data;
-}                   t_list;
+}                   t_list; */
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
@@ -25,4 +26,5 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 		cur = *begin_list;
 		ft_list_remove_if(&cur->next, data_ref, cmp);
 	}
+
 }
