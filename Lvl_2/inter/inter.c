04f717aca8daea5948785d5f6e7011246317f40d
Leo Santander <santandernycz.ls@gmail.com>
2025-11-13 13:54:44 +0000

    fix inter

--- Lvl_2/inter/inter.c
+++ Lvl_2/inter/inter.c
@@ -10,17 +10,17 @@ int	iter(char *str, char c, int len)
 	return (0);
 }
 
-int	main(int argc, char *argv[])
+int	main(int ac, char **av)
 {
 	int	i;
 
-	if (argc == 3)
+	if (ac == 3)
 	{
 		i = 0;
-		while (argv[1][i])
+		while (av[1][i])
 		{
-			if (!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
-				write(1, &argv[1][i], 1);
+			if (!iter(av[1], av[1][i], i) && iter(av[2], av[1][i], -1))
+				write(1, &av[1][i], 1);
 			i += 1;
 		}
 	}
